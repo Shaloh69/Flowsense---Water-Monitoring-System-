2fea1e8b0fa7701c930bda439e05c9982729e126
Shem Joshua M. Dumpor <dumporshemjoshua@gmail.com>
2026-04-15 17:14:17 +0800

    78

--- client/pages/bills.tsx
+++ client/pages/bills.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Spinner } from "@heroui/spinner";
 import { Divider } from "@heroui/divider";
